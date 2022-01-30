6f6fea930e41725fd6463f8ae75782a2632219bb
ryan-doan <doanngocdung01@gmail.com>
2022-01-30 15:09:41 -0500

    add audio to day 7

--- Assets/QuitGame.cs
+++ Assets/QuitGame.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
+    [SerializeField] private float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if (time >= 10.0f)
+        {
+            quit();
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void quit()
     {
-        if (string.Equals(other.name, "Player"))
-        {
-            Application.Quit();
-            UnityEditor.EditorApplication.isPlaying = false;
-        }
+
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
